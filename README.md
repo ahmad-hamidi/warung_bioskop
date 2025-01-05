7dac14a298cff6ad85b17ca10e6aed524d9a9b32
ahmadhamidi <ahmadhamidi@assetfindr.com>
2025-01-05 09:58:20 +0700

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# warung_bioskop
+
+Warung Bioskop is provide movie list and ticketing simulation
---
+++ android/app/src/main/kotlin/com/warung/bioskop/warung_bioskop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.warung.bioskop.warung_bioskop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
