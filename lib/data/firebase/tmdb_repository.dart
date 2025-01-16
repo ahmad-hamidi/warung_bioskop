77880416f6088b3f07a5a1ff14f412a816a29aa6
ahmadhamidi <ahmadhamidi@assetfindr.com>
2025-01-16 22:34:24 +0700

    setup nowPlaying and upComing repository

--- lib/data/firebase/tmdb_repository.dart
+++ lib/data/firebase/tmdb_repository.dart
@@ -11,6 +11,11 @@ class TmdbRepository implements MovieRepository {
   final String accessToken =
       Aeshelper().decryptText('${dotenv.env['TMBD_TOKEN']}');
 
+  late final options = Options(headers: {
+    'Authorization': 'Bearer $accessToken',
+    'accept': 'application/json'
+  });
+
   TmdbRepository({Dio? d}) : dio = d ?? Dio();
 
   @override
@@ -26,14 +31,40 @@ class TmdbRepository implements MovieRepository {
   }
 
   @override
-  Future<Result<List<Movie>>> getNowPlaying({int page = 1}) {
-    // TODO: implement getNowPlaying
-    throw UnimplementedError();
-  }
+  Future<Result<List<Movie>>> getNowPlaying({int page = 1}) async =>
+      _getMovies(_MovieCategory.nowPlaying.toString(), page: page);
 
   @override
-  Future<Result<List<Movie>>> getUpcoming({int page = 1}) {
-    // TODO: implement getUpcoming
-    throw UnimplementedError();
+  Future<Result<List<Movie>>> getUpcoming({int page = 1}) async =>
+      _getMovies(_MovieCategory.upcoming.toString(), page: page);
+
+  Future<Result<List<Movie>>> _getMovies(String category,
+      {int page = 1}) async {
+    try {
+      final response = await dio?.get(
+          'https://api.themoviedb.org/3/movie/$category?page=$page',
+          options: options);
+
+      if (response?.statusCode == 200) {
+        final list = List<Map<String, dynamic>>.from(response!.data['results']);
+        return Result.success(list.map((e) => Movie.fromJSON(e)).toList());
+      } else {
+        return const Result.failed('Failed GET Movie');
+      }
+    } on DioException catch (e) {
+      return Result.failed('${e.message}');
+    }
   }
+}
+
+enum _MovieCategory {
+  nowPlaying('now_playing'),
+  upcoming('upcoming');
+
+  final String string;
+
+  const _MovieCategory(String instring) : string = instring;
+
+  @override
+  String toString() => string;
 }
